668763cab4b49da9dd7d9dcacd7acd1bfef9a572
Nikola <nikola.cvetkovic91@gmail.com>
2018-10-08 21:22:21 +0200

    add POJOs

---
+++ src/main/java/xyz/riocode/guruspring/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package xyz.riocode.guruspring.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/xyz/riocode/guruspring/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package xyz.riocode.guruspring.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
