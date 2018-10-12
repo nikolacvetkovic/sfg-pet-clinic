59b87bf74e79b505725525f071db17a0fa8ee91c
Nikola <nikola.cvetkovic91@gmail.com>
2018-10-12 12:28:40 +0200

    refactoring to common crud interface

--- pet-clinic-data/src/main/java/xyz/riocode/guruspring/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/xyz/riocode/guruspring/sfgpetclinic/services/VetService.java
@@ -2,10 +2,6 @@ package xyz.riocode.guruspring.sfgpetclinic.services;
 
 import xyz.riocode.guruspring.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long>{
     Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
