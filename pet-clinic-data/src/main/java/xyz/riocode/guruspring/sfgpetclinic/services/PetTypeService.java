a9c09c6a4bbf7e3609306845ad14da0957665340
Nikola <nikola.cvetkovic91@gmail.com>
2018-10-22 08:37:30 +0200

    add PetType service and implement PetTypeServiceMap

---
+++ pet-clinic-data/src/main/java/xyz/riocode/guruspring/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package xyz.riocode.guruspring.sfgpetclinic.services;
+
+import xyz.riocode.guruspring.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
