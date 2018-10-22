ff9adc122e6e4579df371d6f51a1b3d77bcede0e
Nikola <nikola.cvetkovic91@gmail.com>
2018-10-22 11:19:07 +0200

    fix 'find owners' broken link

--- pet-clinic-web/src/main/java/xyz/riocode/guruspring/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/xyz/riocode/guruspring/sfgpetclinic/controllers/OwnerController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import xyz.riocode.guruspring.sfgpetclinic.services.OwnerService;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
@@ -14,11 +15,16 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    @RequestMapping({"/", "/index", "/index.html"})
     public String listOwners(Model model){
         model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
+    }
+
+    @RequestMapping("/find")
+    public String findOwners(){
+        return "notimplemented";
     }
 
 }
