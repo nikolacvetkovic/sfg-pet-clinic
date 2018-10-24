f52b91541e7fc7667d7986591cec457dbaf3f66e
Nikola <nikola.cvetkovic91@gmail.com>
2018-10-24 21:13:37 +0200

    add map service for visit

---
+++ pet-clinic-data/src/main/java/xyz/riocode/guruspring/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package xyz.riocode.guruspring.sfgpetclinic.services;
+
+import xyz.riocode.guruspring.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}
