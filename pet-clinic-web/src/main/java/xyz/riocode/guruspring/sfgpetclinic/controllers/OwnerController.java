c382925b72eabab5d353e7bd3f99a186247297d9
Nikola <nikola.cvetkovic91@gmail.com>
2018-12-03 15:17:40 +0100

    add toString method, implement new handler in OwnerController, add html pages for owners

--- pet-clinic-web/src/main/java/xyz/riocode/guruspring/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/xyz/riocode/guruspring/sfgpetclinic/controllers/OwnerController.java
@@ -2,7 +2,10 @@ package xyz.riocode.guruspring.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import xyz.riocode.guruspring.sfgpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
@@ -27,4 +30,11 @@ public class OwnerController {
         return "notimplemented";
     }
 
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable int ownerId){
+        ModelAndView modelAndView = new ModelAndView("owners/ownerDetails");
+        modelAndView.addObject(ownerService.findById(Long.valueOf(ownerId)));
+
+        return modelAndView;
+    }
 }
