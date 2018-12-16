299fc225c12a7f96f953a7697a380e2c6be9587b
Nikola <nikola.cvetkovic91@gmail.com>
2018-12-16 21:13:47 +0100

    implement handler to return json list of vets

--- pet-clinic-web/src/main/java/xyz/riocode/guruspring/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/xyz/riocode/guruspring/sfgpetclinic/controllers/VetController.java
@@ -2,8 +2,13 @@ package xyz.riocode.guruspring.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import xyz.riocode.guruspring.sfgpetclinic.model.Vet;
 import xyz.riocode.guruspring.sfgpetclinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -19,6 +24,11 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 
 }
