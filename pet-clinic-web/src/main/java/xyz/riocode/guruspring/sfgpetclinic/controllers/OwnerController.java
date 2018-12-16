e54e25bf5e562ab515d90e4ae64fffd7cbffeacd
Nikola <nikola.cvetkovic91@gmail.com>
2018-12-16 19:37:21 +0100

    implement create/edit visit

--- pet-clinic-web/src/main/java/xyz/riocode/guruspring/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/xyz/riocode/guruspring/sfgpetclinic/controllers/OwnerController.java
@@ -3,10 +3,8 @@ package xyz.riocode.guruspring.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import xyz.riocode.guruspring.sfgpetclinic.model.Owner;
 import xyz.riocode.guruspring.sfgpetclinic.services.OwnerService;
@@ -24,17 +22,10 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-//    @InitBinder
-//    public void setAllowedFields(WebDataBinder dataBinder){
-//        dataBinder.setDisallowedFields("id");
-//    }
-
-//    @GetMapping({"", "/", "/index", "/index.html"})
-//    public String listOwners(Model model){
-//        model.addAttribute("owners", ownerService.findAll());
-//
-//        return "owners/index";
-//    }
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
+    }
 
     @GetMapping("/find")
     public String findOwners(Model model){
---
+++ pet-clinic-web/src/main/java/xyz/riocode/guruspring/sfgpetclinic/controllers/VisitController.java
@@ -0,0 +1,57 @@
+package xyz.riocode.guruspring.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import xyz.riocode.guruspring.sfgpetclinic.model.Pet;
+import xyz.riocode.guruspring.sfgpetclinic.model.Visit;
+import xyz.riocode.guruspring.sfgpetclinic.services.PetService;
+import xyz.riocode.guruspring.sfgpetclinic.services.VisitService;
+
+import javax.validation.Valid;
+
+@Controller
+public class VisitController {
+
+    private final VisitService visitService;
+    private final PetService petService;
+
+
+    public VisitController(VisitService visitService, PetService petService) {
+        this.visitService = visitService;
+        this.petService = petService;
+    }
+
+    @InitBinder
+    public void serAllowedFields(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
+    }
+
+    @ModelAttribute("visit")
+    public Visit loadPetWithVisit(@PathVariable Long petId, Model model){
+        Pet pet = petService.findById(petId);
+        model.addAttribute("pet", pet);
+        Visit visit = new Visit();
+        pet.getVisits().add(visit);
+        visit.setPet(pet);
+
+        return visit;
+    }
+
+    @GetMapping("/owners/*/pets/{petId}/visits/new")
+    public String initNewVisitForm(@PathVariable Long petId, Model model){
+        return "pets/createOrUpdateVisitForm";
+    }
+
+    @PostMapping("/owners/*/pets/{petId}/visits/new")
+    public String processNewVisitForm(@Valid Visit visit, BindingResult result, Pet pet){
+        if(result.hasErrors()){
+            return "pets/createOrUpdateVisitForm";
+        } else {
+            visitService.save(visit);
+            return "redirect:/owners/" + pet.getOwner().getId();
+        }
+    }
+}
